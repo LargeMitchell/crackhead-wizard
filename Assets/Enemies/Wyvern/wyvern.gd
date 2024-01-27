b6e01a274dd78a7bb7d5530a51be5b8f358c2f26
FalcoVH <falco.van.haut@gmail.com>
2024-01-27 21:16:57 +0100

    [ADDED] wyvern AI

--- Assets/Enemies/Wyvern/wyvern.gd
+++ Assets/Enemies/Wyvern/wyvern.gd
@@ -3,10 +3,11 @@ extends CharacterBody3D
 @export var health: int = 100
 @export var move_speed: float = 5.5
 @export var attack_damage: int = 10
-@export var attack_range: int = 10
+@export var attack_range: int = 5
 @export var notice_range: int = 20
 
 @onready var player : CharacterBody3D = get_tree().get_first_node_in_group("player")
+@onready var bullet : PackedScene = preload("res://Assets/Projectiles/Bullet.tscn")
 
 var state_timer: float = 0.0
 @export var attack_cd: float = 2.0
@@ -16,13 +17,17 @@ var chase_pos: Vector3 = Vector3(0.0, 0.0, 0.0)
 var rand_height: float = 0.0
 var rand_dist: float = 10.0
 var rand_rot_offset: float = 90.0
-var rot_time: float = 0.0
+var shot_time: float = 0.05
+var shot_timer: float = 0.0
 var rng = RandomNumberGenerator.new()
 
-func _enter_tree():
+func reroll_point():
 	rand_height = rng.randf_range(3.0, 5.0)
 	rand_dist = rng.randf_range(7.5, 12.5)
 	rand_rot_offset = rng.randf() * PI * 2.0
+	
+func _enter_tree():
+	reroll_point()
 
 enum enemy_state 
 {
@@ -37,8 +42,12 @@ func set_state(new_state: enemy_state):
 	state = new_state
 	state_timer = 0.0
 
+var charge_hit_player = false
+
 func _process(delta):
 	if health <= 0:
+		global_position += Vector3(0.0, -delta * 6.0, 0.0)
+		global_rotate(Vector3(0.0, 1.0, 0.0), delta)
 		return
 		
 	if player == null:
@@ -66,24 +75,44 @@ func _process(delta):
 			chase_pos = player.global_position + Vector3(0.0, rand_height, 0.0)
 			
 			if state_timer >= 1.0:
-				set_state(enemy_state.CHARGING)
-				chase_pos = player.global_position
-			
-			#var nx = sin(state_timer * PI * 2.0 + rand_rot_offset)
-			#var nz = cos(state_timer * PI * 2.0 + rand_rot_offset)
-			
+				
+				if randi_range(0, 1) == 0:
+					set_state(enemy_state.CHARGING)
+					chase_pos = player.global_position
+					charge_hit_player = false
+				else:
+					set_state(enemy_state.SHOOTING)
+					var nx = sin(PI * 2.0 + rand_rot_offset)
+					var nz = cos(PI * 2.0 + rand_rot_offset)
+					chase_pos = player.global_position + Vector3(nx * rand_dist, rand_height, nz * rand_dist)
+					
+					reroll_point()
+
 		enemy_state.CHARGING:
 			state_timer += delta
 			
 			if state_timer >= 1.0:	
-				enemy_state.CHASING
-				
-		enemy_state.SHOOTING:
-			
-			#if $AnimatedSprite3D.frame >= 3: # hard coded to make him only shoot once
 				set_state(enemy_state.CHASING)
 				
-	
+		enemy_state.SHOOTING:
+			state_timer += delta
+			shot_timer += delta
+			
+			if shot_timer >= shot_time:
+				
+				shot_timer -= shot_time
+				
+				var p = bullet.instantiate()
+				add_child(p)
+				p.global_transform.origin = global_transform.origin + Vector3.UP
+				p.direction = Vector3((player.global_position + (-Vector3.UP)) - global_position) + Vector3(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized()
+				p.direction = p.direction.normalized()
+							
+			$".".look_at(player.global_position, Vector3.UP)
+			
+			if state_timer >= 2.5:
+				set_state(enemy_state.CHASING)
+				
 
 func _physics_process(delta):
 	if health <= 0:
@@ -92,21 +121,18 @@ func _physics_process(delta):
 	if player == null:
 		return
 		
+	if state == enemy_state.CHARGING && !charge_hit_player:
+		
+		if global_position.distance_to(player.global_position) <= 5.0:
+			player.take_damage(attack_damage)
+			charge_hit_player = true
+	
 	var dir = chase_pos - global_position
 	dir = dir.normalized()
 
 	velocity = dir * move_speed
 	move_and_slide()
-	
-
-func attack_player():
-	var eye_line = Vector3.UP * 1.5
-	var query = PhysicsRayQueryParameters3D.create(global_position+eye_line, player.global_position+eye_line, 1)
-	var result = get_world_3d().direct_space_state.intersect_ray(query)
-	
-	if result.is_empty():
-		print("ouch!")
-		# player.take_damage(attack_damage)
+		
 		
 func take_damage(damage_amount:int):
 	health -= damage_amount
